e0597f8bd22f05505a7cb1d219c7ee032aec250f
achi3v3 <amir-tutaev@mail.ru>
2025-01-21 16:35:52 +0500

    feat(back): make new telegram bot

---
+++ src/package/NewBot.go
@@ -0,0 +1,37 @@
+package funcExcel
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/go-telegram/bot"
+	_ "github.com/lib/pq"
+)
+
+func NewBot() {
+	launchNewBot()
+}
+
+func launchNewBot() {
+	databaseHandler()
+
+	token := BotToken()
+
+	fmt.Println(token)
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	opts := []bot.Option{
+		bot.WithDefaultHandler(UniversalHandler),
+	}
+
+	b, err := bot.New(token, opts...)
+	if err != nil {
+		panic(err)
+	}
+	b.RegisterHandlerMatchFunc(isSnStartSchedule, snStartSchedule)
+
+	b.Start(ctx)
+}
