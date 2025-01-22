f48f17b8ec5d0022f7de166ecf5d456c92353ffd
achi3v3 <amir-tutaev@mail.ru>
2025-01-22 11:21:37 +0500

    feat(back): add start and file upload handlers

--- src/package/commands.go
+++ src/package/commands.go
@@ -2,13 +2,14 @@ package funcExcel
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
 )
 
 const (
-	start           = "start"
+	start           = "/start"
 	snstartSchedule = "/snstart_schedule"
 	uploadFile      = "/snupload_schedule"
 
@@ -32,28 +33,61 @@ func snStartSchedule(ctx context.Context, b *bot.Bot, update *models.Update) {
 	setUserState(chatID, "group", "")
 	setUserState(chatID, "day", "")
 
-}
+	userId := update.Message.Chat.ID
+	username := update.Message.From.Username
+	firstname := update.Message.From.FirstName
+	flag := addUser(userId, username, firstname)
+	if flag {
+		if username == "" {
+			username = "none"
+		}
+		if firstname == "" {
+			firstname = "none"
+		}
+		b.SendMessage(ctx, &bot.SendMessageParams{
+			ChatID:    idOwner,
+			Text:      fmt.Sprintf("<b>✅ Новый пользователь</b>\n\n<blockquote>id: %d</blockquote>\nusername: @%s\n<blockquote>firstname: %s</blockquote>\n", userId, username, firstname),
+			ParseMode: models.ParseModeHTML,
+		})
+	}
 
+}
 func isUploadFile(update *models.Update) bool {
 	return update.Message != nil && update.Message.Text == uploadFile
 }
 
-// func snUploadSchedule(ctx context.Context, b *bot.Bot, update *models.Update) {
+func snUploadFile(ctx context.Context, b *bot.Bot, update *models.Update) {
 
-// 	chatID := update.Message.Chat.ID
-// 	if chatID != idOwner {
-// 		sendMsgError(ctx, b, update)
-// 	}
-// 	sendCourseSelection(ctx, b, chatID)
+	chatID := update.Message.Chat.ID
+	if chatID != idOwner {
+		b.SendMessage(ctx, &bot.SendMessageParams{
+			ChatID: chatID,
+			Text:   "🔞 Недостаточно прав.",
+		})
+		return
+	}
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID:    chatID,
+		Text:      "✳️ Загрузка расписания:\n<blockquote>Файл: <i>File.xlsx</i></blockquote>\n<blockquote>Неделя: <i>17</i></blockquote>",
+		ParseMode: models.ParseModeHTML,
+	})
 
-// 	userStates.Lock()
-// 	if _, exists := userStates.data[chatID]; !exists {
-// 		userStates.data[chatID] = make(map[string]string)
-// 	}
-// 	userStates.Unlock()
+	userStates.Lock()
+	if _, exists := userStates.data[chatID]; !exists {
+		userStates.data[chatID] = make(map[string]string)
+	}
+	userStates.Unlock()
 
-// }
+}
 
 func isStart(update *models.Update) bool {
 	return update.Message != nil && update.Message.Text == start
+}
+func Start(ctx context.Context, b *bot.Bot, update *models.Update) {
+	chatID := update.Message.Chat.ID
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID:    chatID,
+		Text:      "🏛 <a href=\"https://t.me/sn_schedulebot\">Schedule Bot</a> (⚙️ Стадия-разработки)\n\nПоиск расписания — /snstart_schedule ",
+		ParseMode: models.ParseModeHTML,
+	})
 }
