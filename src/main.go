bf328f6675061f92e0a07bc3b9b010075c05674b
achi3v3 <amir-tutaev@mail.ru>
2025-01-23 17:45:38 +0500

    feat(back): add ask the dbwriter

--- src/main.go
+++ src/main.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"fmt"
-	funcExcel "src/package"
+	functions "schedule/snschedule_bot"
+	"strings"
+	"time"
 )
 
 func main() {
 	fmt.Println("func: main.go")
-	funcExcel.FunctionScheduleBot()
+
+	var answer string
+	fmt.Println("ЗАПУСКАТЬ ПОЛНУЮ ЗАГРУЗКУ БД? (БАЗА ДАННЫХ ДОЛЖНА БЫТЬ ПУСТАЯ){КОНЕЧНО/СКИП}")
+	fmt.Scan(&answer)
+	if strings.ToLower(answer) == "конечно" {
+		fmt.Println("ПОЛНАЯ ЗАГРУЗКА БД (ПЕРЕД ЗАПУСКОМ ОЖИДАНИЕ 10 СЕКУНД)")
+		// functions.FunctionDbWriter()
+		time.Sleep(10 * time.Second)
+	}
+	functions.NewBot()
 }
