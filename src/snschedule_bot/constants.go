fa21e8df85b5ea0bb8588e3c6036e6861d7bd624
achi3v3 <amir-tutaev@mail.ru>
2025-05-10 23:01:18 +0500

    feat(back): create file for const's

---
+++ src/snschedule_bot/constants.go
@@ -0,0 +1,21 @@
+package functions
+
+const (
+	// Common message parts
+	BotHeader = "🏛 <a href=\"https://t.me/sn_schedulebot\">Schedule Bot</a> — Расписание (⚙️ Бета-версия)"
+	WeekInfo  = "📆 Установленная неделя: %s"
+
+	// Course and group info
+	CourseInfo = "\n\nУровень обучения: %s"
+	GroupInfo  = "\nГруппа: %s"
+
+	// Page info
+	PageInfo = "\n\n<i>Лист %d:</i>"
+
+	// Common buttons
+	BackButton = "Назад"
+
+	// Page sizes
+	PageSize        = 8
+	PageSizeNotices = 3
+)
